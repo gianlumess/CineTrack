46c2962f06747fdeaa8a5adfba25b77d7f9f3ba1
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-09-30 16:49:29 +0200

    Initial commit

---
+++ src/main/java/gianlucamessina/CineTrack/CineTrackApplication.java
@@ -0,0 +1,13 @@
+package gianlucamessina.CineTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineTrackApplication.class, args);
+	}
+
+}
